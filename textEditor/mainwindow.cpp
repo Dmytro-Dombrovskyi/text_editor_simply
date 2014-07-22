191799527e8c9467b42631fa22cfb917d219023b
dmytro_dombrovskyi <dmytro_dombrovskyi@yahoo.com>
2014-07-22 10:33:51 +0300

    add png icons and actions

--- textEditor/mainwindow.cpp
+++ textEditor/mainwindow.cpp
@@ -1,14 +1,41 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    new_file_ = false;
+    file_opened_ = false;
+    file_changed_ = false;
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionNew_triggered()
+{
+
+}
+
+void MainWindow::on_actionQuit_triggered()
+{
+    file_changed_ = true;
+    if(file_opened_ && file_changed_)
+    {
+        QMessageBox::StandardButton answer =
+                QMessageBox::question(this, "Quit dialog",
+                              "Do you really want to quit?",
+                              QMessageBox::Yes | QMessageBox::No);
+        if(QMessageBox::Yes == answer)
+            QApplication::instance()->quit();
+    }
+    else
+        QApplication::instance()->quit();
 }
--- textEditor/mainwindow.h
+++ textEditor/mainwindow.h
@@ -15,8 +15,17 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_actionNew_triggered();
+
+    void on_actionQuit_triggered();
+
 private:
     Ui::MainWindow *ui;
+
+    bool new_file_;
+    bool file_opened_;
+    bool file_changed_;
 };
 
 #endif // MAINWINDOW_H
